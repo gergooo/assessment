cb63f5f6e411ad31e53d665588fabaebcebc12f1
gergooo <39014407+gergooo@users.noreply.github.com>
2021-01-14 10:51:32 +0100

    converting numbers below hundred

--- src/number-to-phrase.js
+++ src/number-to-phrase.js
@@ -1,5 +1,47 @@
-function getNumberPhrase() {
-  return 'three';
+const digitNames = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+];
+
+const numbersBetween10and20 = {
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+};
+
+const tenMultiples = {
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+};
+
+function getNumberPhrase(number) {
+  return (
+    tenMultiples[number] ||
+    numbersBetween10and20[number] ||
+    digitNames[number] ||
+    `${tenMultiples[number - (number % 10)]}-${digitNames[number % 10]}`
+  );
 }
 
 module.exports = { getNumberPhrase };
--- src/number-to-phrase.unit.test.js
+++ src/number-to-phrase.unit.test.js
@@ -3,7 +3,51 @@ require('chai').should();
 const { getNumberPhrase } = require('./number-to-phrase');
 
 describe('Number to phrase - unit tests', function () {
-  it('3', function () {
+  it('numbers below ten', function () {
+    getNumberPhrase(0).should.equal('zero');
+    getNumberPhrase(1).should.equal('one');
+    getNumberPhrase(2).should.equal('two');
     getNumberPhrase(3).should.equal('three');
+    getNumberPhrase(4).should.equal('four');
+    getNumberPhrase(5).should.equal('five');
+    getNumberPhrase(6).should.equal('six');
+    getNumberPhrase(7).should.equal('seven');
+    getNumberPhrase(8).should.equal('eight');
+    getNumberPhrase(9).should.equal('nine');
+  });
+
+  it('numbers below twenty', function () {
+    getNumberPhrase(10).should.equal('ten');
+    getNumberPhrase(11).should.equal('eleven');
+    getNumberPhrase(12).should.equal('twelve');
+    getNumberPhrase(13).should.equal('thirteen');
+    getNumberPhrase(14).should.equal('fourteen');
+    getNumberPhrase(15).should.equal('fifteen');
+    getNumberPhrase(16).should.equal('sixteen');
+    getNumberPhrase(17).should.equal('seventeen');
+    getNumberPhrase(18).should.equal('eighteen');
+    getNumberPhrase(19).should.equal('nineteen');
+  });
+
+  context('numbers from 20 to 100', function () {
+    it('multiples of 10', function () {
+      getNumberPhrase(20).should.equal('twenty');
+      getNumberPhrase(30).should.equal('thirty');
+      getNumberPhrase(40).should.equal('forty');
+      getNumberPhrase(50).should.equal('fifty');
+      getNumberPhrase(60).should.equal('sixty');
+      getNumberPhrase(70).should.equal('seventy');
+      getNumberPhrase(80).should.equal('eighty');
+      getNumberPhrase(90).should.equal('ninety');
+    });
+
+    it('twenties and above', function () {
+      getNumberPhrase(21).should.equal('twenty-one');
+      getNumberPhrase(23).should.equal('twenty-three');
+      getNumberPhrase(35).should.equal('thirty-five');
+      getNumberPhrase(59).should.equal('fifty-nine');
+      getNumberPhrase(64).should.equal('sixty-four');
+      getNumberPhrase(98).should.equal('ninety-eight');
+    });
   });
 });
